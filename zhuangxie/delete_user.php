693ea6ffba3326c0d83a8a34962b97ebabf628ec
newortao <112098578+newortao@users.noreply.github.com>
2025-07-24 11:05:39 +0800

    Update delete_user.php

--- zhuangxie/delete_user.php
+++ zhuangxie/delete_user.php
@@ -2,10 +2,12 @@
 include 'includes/auth.php';
 include 'db.php';
 
-if (!isAdmin()) {
+if (!checkSessionTimeout() || !isAdmin()) {
     $_SESSION['error'] = "无权执行此操作";
     redirect('manage_users.php');
 }
+
+refreshSession();
 
 $user_id = $_GET['id'] ?? 0;
 
